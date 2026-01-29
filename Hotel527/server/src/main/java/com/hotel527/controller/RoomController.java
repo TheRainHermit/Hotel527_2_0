acde9e3972a58c135ac98e2e5e77c6234dd5ee65
TheRainHermit <miguel.fabra211294@gmail.com>
2026-01-28 21:44:21 -0500

    C1: Modelo Room

---
+++ Hotel527/server/src/main/java/com/hotel527/controller/RoomController.java
@@ -0,0 +1,23 @@
+package com.hotel527.controller;
+
+import com.hotel527.model.Room;
+import com.hotel527.service.RoomService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rooms")
+@CrossOrigin(origins = "*")
+public class RoomController {
+    private final RoomService roomService;
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    public List<Room> getRooms() {
+        return roomService.getAllRooms();
+    }
+}
---
+++ Hotel527/server/src/main/java/com/hotel527/service/RoomService.java
@@ -0,0 +1,19 @@
+package com.hotel527.service;
+
+import com.hotel527.model.Room;
+import com.hotel527.repository.RoomRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class RoomService {
+    private final RoomRepository roomRepository;
+
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
+}
